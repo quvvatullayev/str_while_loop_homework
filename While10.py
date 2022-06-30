ec17950b4949bba9aac099c22f956d820c46aa5e
tajriba <quvvatullayev@gmail.com>
2022-06-30 10:36:21 +0300

    While10.py

--- While10.py
+++ While10.py
@@ -6,4 +6,17 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    sum = 0
+    i = 0
+    n = len(s)
+    s = int(s)
+    
+    while i < n:
+        if (s%10)%2 == 1:
+            sum += s%10
+        s //= 10
+        i += 1
+
+    return sum
+
+print(main("75984235"))
