75dc1f411262276a457e3726a11f7cd903f27572
tajriba <quvvatullayev@gmail.com>
2022-06-30 09:32:37 +0300

    .\While06.py

--- While06.py
+++ While06.py
@@ -7,3 +7,22 @@ def main(s):
     Returns:
         int: return answer
     """
+    s = str(s)
+    i = 0
+    sum = 0
+    while i < len(s):
+
+        if s[i].isalpha():
+
+            if s[i].lower() == "a" or s[i].lower() == "o" or s[i].lower() == "e" or\
+                s[i].lower() == "i" or s[i].lower() == "u":
+                sum += 0
+            
+            else:
+                sum += 1
+
+        i += 1
+
+    return sum
+
+print(main("odevfsd"))
